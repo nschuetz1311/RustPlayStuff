f90090455591d886bfbd1e1a6aae9892fea8c3ba
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-04-13 18:50:11 +0200

    added a basic "for" loop

--- conditions/src/main.rs
+++ conditions/src/main.rs
@@ -2,4 +2,11 @@ fn main() {
     let is_weekend:bool = true;
     let activity = if is_weekend {"Hiking"} else {"Working"};
     println!("{} is todays activity", activity);
+
+
+    /*For loop */
+    let arr = [10, 12, 14, 18, 24];
+    for element in arr {
+        println!("{}", element)
+    }
 }

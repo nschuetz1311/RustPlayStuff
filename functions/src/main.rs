7f5f532b680b0284de27e1263937944e1d36047c
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-04-15 09:29:21 +0200

    Initial commit for functions chapter

---
+++ functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+fn calculate_sum(a:i32, b:i32) -> i32 {
+    let sum = a + b;        // statements: line has to be terminated by ';'
+    sum                     // expression: no termination of the line as this will return the value
+}

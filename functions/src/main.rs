55c7a3f8dcb106ad370c03d5644d20d72ecf614f
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-04-15 09:58:59 +0200

    first attempt at function to translate scores into grades

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let scores_array: [i8; 5] = [85, 90, 78, 92, 88];
+    for individual_score in scores_array {
+        println!("{}",get_letter_grade(individual_score));
+    }
 }
 
 
 fn calculate_sum(a:i32, b:i32) -> i32 {
     let sum = a + b;        // statements: line has to be terminated by ';'
     sum                     // expression: no termination of the line as this will return the value
+}
+
+fn get_letter_grade(students_score:i8) -> char {
+    let students_grade:char;
+    if students_score >= 90 {
+        students_grade = 'A';
+    } else if ((students_score < 90) && (students_score > 79)) {
+        students_grade = 'B';
+    } else if ((students_score < 80) && (students_score > 69)) {
+        students_grade = 'c';
+    } else if ((students_score < 70) && (students_score > 59)) {
+        students_grade = 'D';
+    } else {
+        students_grade = 'F';
+    }
+    students_grade
 }

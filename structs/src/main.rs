3d118b72c2f8fc4e536223376861957f162d2d10
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-05-07 13:37:26 +0200

    Task 1. Define a Book struct with fields for the title, author, and number of pages. 2. Implement a method on the Book struct: get_summary to return a summary of the book as a string. 3. In the main function, create an array of books. 4. Use a for loop to iterate through the array and print the summary of each book using the get_summary method.

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -23,6 +23,21 @@ impl Rectangle{
     }
 }
 
+// 1. Define a Book struct with fields for the title, author, and number of pages.
+struct Book{
+    title:String,
+    author:String,
+    number_of_pages:u32,
+}
+
+// 2. Implement a method on the Book struct:
+//     get_summary to return a summary of the book as a string.
+impl Book{
+    fn get_summary(&self)->String{
+        format!("{}, was written by {} and has {} pages",self.title, self.author, self.number_of_pages)
+    }
+}
+
 fn main() {
     let user1 = User{
         name:String::from("Niko"),
@@ -40,4 +55,33 @@ fn main() {
         height:15,
     };
     println!("the area of the rectangle is: {}", shape1.area());
+
+    let name = "Jane";
+    let age = 30; 
+    let introduction = format!("Hi, my name is {} and I am {} years old.", name, age); 
+    println!("{}", introduction); 
+
+    // 3. In the main function, create an array of books.
+    let book_array = [
+        Book {
+            title: String::from("Book1"),
+            author: String::from("Author1"),
+            number_of_pages: 500,
+        },
+        Book {
+            title: String::from("Book2"),
+            author: String::from("Author2"),
+            number_of_pages: 400,
+        },
+        Book {
+            title: String::from("Book3"),
+            author: String::from("Author3"),
+            number_of_pages: 170,
+        },
+    ];
+
+    // 4. Use a for loop to iterate through the array and print the summary of each book using the get_summary method.
+    for book in book_array {
+        println!("{}",book.get_summary());
+    }
 }

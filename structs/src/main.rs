7e38cfaf4884f72fd215462e98bd3137b3c9c2ad
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-05-07 13:14:31 +0200

    methods to inject one or multiple functions into a structure use  'impl' with the name of the structure. this can contain one or more function which have t0 be declared as normal functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,6 +12,17 @@ fn greet_user(user:&User) {
 struct Color (i32,i32,i32); // RGB values
 struct Point (f64,f64,f64); // xyz coordinates
 
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle{
+    fn area(&self)->u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
     let user1 = User{
         name:String::from("Niko"),
@@ -24,5 +35,9 @@ fn main() {
 
     let red = Color(0xff,0,0);
     let p1 = Point(100.9, 25.6, 37.6);
-
+    let shape1 = Rectangle{
+        width:5,
+        height:15,
+    };
+    println!("the area of the rectangle is: {}", shape1.area());
 }

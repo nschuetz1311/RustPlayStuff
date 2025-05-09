c426433f359cdeb8717c1490403c71ff1399d798
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-05-09 20:48:06 +0200

    modified enums
    Enums with values to compute in the match section

--- Enums/src/main.rs
+++ Enums/src/main.rs
@@ -4,6 +4,20 @@ enum TrafficLight {
     Green
 }
 
+/* Enums with Values */
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+    Square(f64),
+}
+
+fn calculate_area (shape:Shape) {
+    match shape {
+        Shape::Circle(radius) => println!("the area of the circle is {}", 3.14 * radius * radius),
+        Shape::Rectangle(width, height) => println!("the area of the rectangle is {}", width * height),
+        Shape::Square(side) => println!("the are of the square is {}", side * side),
+    }
+}
 
 fn main() {
     let light = TrafficLight::Red;
@@ -13,4 +27,8 @@ fn main() {
         TrafficLight::Yellow => println!("Be prepared to stop"),
         TrafficLight::Green => println!("Go!"),
     }
+
+    let shape1 = Shape::Rectangle(30.0, 17.1);
+
+    calculate_area(shape1);
 }

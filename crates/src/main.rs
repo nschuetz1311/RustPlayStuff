3d01f454e221c7944fd0986d8a401a302112b172
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-05-24 14:43:05 +0200

    course: Crates
    first implementation of a lib.
    Added rng-lib to create a random number
    
    Signed-off-by: Nikolas Schuetz <n.schuetz@hotmail.de>

--- crates/src/main.rs
+++ crates/src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    // random number generator
+    let mut rng = rand::rng();
+
+    // range in the braces is displayed by x..y
+    // where x is the starting number and y the last possible number
+    let random_number:u32 = rng.random_range(1..1000);
+
+    println!("the generated number is: {random_number}");
 }

f1e0582f0e90886843b25962d96403e38591fdc7
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-06-19 08:58:50 +0200

    course: Vectors
    create a vector using macro style
    
    Signed-off-by: Nikolas Schuetz <n.schuetz@hotmail.de>

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -7,4 +7,9 @@ fn main() {
     numbers.push(12);
 
     println!("{:?}", numbers);
+
+    // creating a vector using the vector macro
+
+    let vec_m_numbers = vec![1,2,3,4,5];
+    println!("{:?}", vec_m_numbers);
 }

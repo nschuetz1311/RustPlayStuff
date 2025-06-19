0c0f53a1cb4c55db29c78e7c4b7343ffcf1c1104
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-06-19 10:55:13 +0200

    course: HashMaps
    Modifying content of the hashmap
    
    Signed-off-by: Nikolas Schuetz <n.schuetz@hotmail.de>

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -10,13 +10,18 @@ fn main() {
     population.insert("London", 17400100);
     population.insert("Munich", 7400100);
 
-    println!("{:?}", population);
-
-
     // Match with the Key will return only the Value of the Pair if a match is found
     // therefore the var in some will be the population number in my case.
     match population.get("Tokyo") {
         Some(&population_number) => println!("population of Tokyo:  {}", population_number),
         None => println!("No Match found for Tokyo"),
     }
+
+    // Updating the Population of Tokyo
+    population.insert("Tokyo", 47000000);
+    println!("{:?}", population);
+
+    // Removing entry from HashMap
+    population.remove("Tokyo");
+    println!("{:?}", population);
 }

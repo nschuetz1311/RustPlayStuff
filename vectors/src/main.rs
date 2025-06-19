5248b5f1db0ff60c734bd5b27ae029345f45da5d
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-06-19 09:05:54 +0200

    course: Vectors
    fist use of push and pop.
    push adds to the end of the vector where pop removes the last entry and returns teh removed value.
    
    Signed-off-by: Nikolas Schuetz <n.schuetz@hotmail.de>

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -12,4 +12,14 @@ fn main() {
 
     let vec_m_numbers = vec![1,2,3,4,5];
     println!("{:?}", vec_m_numbers);
+
+    let mut fruits = vec!["apple", "bannana", "orange"];
+
+    // add an element
+    fruits.push("grape");
+    println!("{:?}", fruits);
+
+    // remove an element
+    let removed_fruit = fruits.pop();
+    println!("{:?}: removed {:?}", fruits, removed_fruit.unwrap());
 }
